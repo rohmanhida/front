21d5623c493d3c5b550e974db698d1e0cb0823b6
rohmanhida <hodyjones99@gmail.com>
2025-04-18 11:17:48 +0700

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,134 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+
+export default function ShipmentsPage() {
+  const [armadas, setArmadas] = useState([]);
+  const [orders, setShippingOrders] = useState([]);
+  const [shipments, setShipments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchArmadas() {
+      try {
+        const res = await fetch("http://localhost:8000/api/armada");
+        const data = await res.json();
+        setArmadas(data);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching shipments:", err);
+        setLoading(false);
+      }
+    }
+    async function fetchShippingOrders() {
+      try {
+        const res = await fetch("http://localhost:8000/api/shippingorder");
+        const data = await res.json();
+        setShippingOrders(data);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching shipments:", err);
+        setLoading(false);
+      }
+    }
+    async function fetchShipments() {
+      try {
+        const res = await fetch("http://localhost:8000/api/shipment");
+        const data = await res.json();
+        setShipments(data);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching shipments:", err);
+        setLoading(false);
+      }
+    }
+
+    fetchArmadas();
+    fetchShippingOrders();
+    fetchShipments();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <h1>Armada List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Armada ID</th>
+            <th>Type</th>
+            <th>Availability</th>
+            <th>Capacity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {armadas.map((armada) => (
+            <tr key={armada.armada_id}>
+              <td>{armada.armada_id}</td>
+              <td>{armada.type}</td>
+              <td>{armada.availability ? "Available" : "Not Available"}</td>
+              <td>{armada.capacity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h1>Shipping Orders</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Name</th>
+            <th>Vehicle Type</th>
+            <th>Date</th>
+            <th>Detail</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <tr key={order.order_id}>
+              <td>{order.order_id}</td>
+              <td>{order.name}</td>
+              <td>{order.vehicle_type}</td>
+              <td>{order.date}</td>
+              <td>{order.detail}</td>
+              <td>{order.order_status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h1>Shipments</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Shipment ID</th>
+            <th>Armada ID</th>
+            <th>Shipping Order ID</th>
+            <th>Date</th>
+            <th>Departure</th>
+            <th>Destination</th>
+            <th>Status</th>
+            <th>Detail</th>
+            <th>Log</th>
+          </tr>
+        </thead>
+        <tbody>
+          {shipments.map((s) => (
+            <tr key={s.shipment_id}>
+              <td>{s.shipment_id}</td>
+              <td>{s.armada_id}</td>
+              <td>{s.shipping_order_id}</td>
+              <td>{s.date}</td>
+              <td>{s.dep_loc}</td>
+              <td>{s.des_loc}</td>
+              <td>{s.status}</td>
+              <td>{s.detail}</td>
+              <td>{s.shipping_log}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
